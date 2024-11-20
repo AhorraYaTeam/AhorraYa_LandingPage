0d89d49120f52aeacffbebefbaacc5523fc45533
DR4X10Z05FD <fabrizio.diaz0424@gmail.com>
2024-11-20 07:11:17 -0500

    Final

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("JavaScript cargado correctamente.");
 });
+function toggleProfileMenu() {
+    const menu = document.getElementById("profile-menu");
+    menu.classList.toggle("show");
+}
+
+// Opcional: Cierra el menú al hacer clic fuera de él
+window.addEventListener("click", function (e) {
+    const menu = document.getElementById("profile-menu");
+    const profileImg = document.querySelector(".profile-img");
+    if (!menu.contains(e.target) && e.target !== profileImg) {
+        menu.classList.remove("show");
+    }
+});
